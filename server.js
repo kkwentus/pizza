b44f8d0053af974126ede3311a5ea521ff19113b
karen <kkwentus@chef.io>
2018-08-24 13:09:16 -0500

    h1

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', (request, response) => {
+   // response.send('<h2>PizzaPizza!</h2>');
+    response.send({
+        size: 'large',
+        toppings: [
+            'pepperoni', 
+            'provel', 
+            'mushrooms']
+    })
+});
+
+app.listen(3000, ()=> {
+    console.log('Pizza baking on port 3000');
+});
