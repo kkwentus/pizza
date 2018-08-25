f65d838c33f79577d1aba2cb6668eabd250dcbb0
karen <kkwentus@chef.io>
2018-08-25 14:50:27 -0500

    h4

--- public/client.js
+++ public/client.js
@@ -1,6 +1,45 @@
-console.log('client-side code running');
+window.addEventListener('load', function() {
 
-const button = document.getElementById('getAllButton');
-button.addEventListener('click', function(e) {
-  console.log('getAllButton was clicked');
+  var webAuth = new auth0.WebAuth({
+    domain: AUTH0_DOMAIN,
+    clientID: AUTH0_CLIENT_ID,
+    redirectUri: AUTH0_CALLBACK_URL,
+    audience: 'https://' + AUTH0_DOMAIN + '/userinfo',
+    responseType: 'token id_token',
+    scope: 'openid',
+    leeway: 60
+  });
+
+  var loginBtn = document.getElementById('pzloginBtn');
+
+  loginBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    webAuth.authorize();
+  });
+
 });
+
+function isAuthenticated() {
+  // Check whether the current time is past the
+  // access token's expiry time
+  var expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+  return new Date().getTime() < expiresAt;
+}
+
+function handleAuthentication() {
+  webAuth.parseHash(function(err, authResult) {
+    if (authResult && authResult.accessToken && authResult.idToken) {
+      window.location.hash = '';
+      setSession(authResult);
+      loginBtn.style.display = 'none';
+      homeView.style.display = 'inline-block';
+    } else if (err) {
+      homeView.style.display = 'inline-block';
+      console.log(err);
+      alert(
+        'Error: ' + err.error + '. Check the console for further details.'
+      );
+    }
+    displayButtons();
+  });
+}
