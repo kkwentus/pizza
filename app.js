32416196764b6d4732f5c79011edf7d8c879c11d
karen <kkwentus@chef.io>
2018-08-26 15:25:18 -0500

    basic1

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 window.addEventListener('load', function() {
+  var userProfile;
+  var content = document.querySelector('.content');
+  content.style.display = 'block';
 
   var webAuth = new auth0.WebAuth({
     domain: AUTH0_DOMAIN,
@@ -6,78 +9,131 @@ window.addEventListener('load', function() {
     redirectUri: AUTH0_CALLBACK_URL,
     audience: 'https://' + AUTH0_DOMAIN + '/userinfo',
     responseType: 'token id_token',
-    scope: 'openid',
+    scope: 'openid profile',
     leeway: 60
   });
 
   var loginStatus = document.querySelector('.container h4');
+  var loginView = document.getElementById('login-view');
   var homeView = document.getElementById('home-view');
+  var profileView = document.getElementById('profile-view');
 
-  var loginBtn = document.getElementById('loginBtn');
+
+  var loginBtn = document.getElementById('userLoginBtn');
+  var logoutBtn = document.getElementById('userLogoutBtn');
+  var orderBtn = document.getElementById('quickOrderBtn');
+  var homeViewBtn = document.getElementById('homeBtn');
+  var profileViewBtn = document.getElementById('profileBtn');
+
+  homeViewBtn.addEventListener('click', function() {
+    homeView.style.display = 'inline-block';
+    profileView.style.display = 'none';
+  });
+
+  profileViewBtn.addEventListener('click', function() {
+    homeView.style.display = 'none';
+    profileView.style.display = 'inline-block';
+    getProfile();
+  });
 
   loginBtn.addEventListener('click', function(e) {
     e.preventDefault();
     webAuth.authorize();
   });
 
-logoutBtn.addEventListener('click', logout);
+  logoutBtn.addEventListener('click', logout);
 
-function setSession(authResult) {
-  // Set the time that the access token will expire at
-  var expiresAt = JSON.stringify(
-    authResult.expiresIn * 1000 + new Date().getTime()
-  );
-  localStorage.setItem('access_token', authResult.accessToken);
-  localStorage.setItem('id_token', authResult.idToken);
-  localStorage.setItem('expires_at', expiresAt);
-}
-
-function logout() {
-  // Remove tokens and expiry time from localStorage
-  localStorage.removeItem('access_token');
-  localStorage.removeItem('id_token');
-  localStorage.removeItem('expires_at');
-  displayButtons();
-}
-
-function isAuthenticated() {
-  // Check whether the current time is past the
-  // access token's expiry time
-  var expiresAt = JSON.parse(localStorage.getItem('expires_at'));
-  return new Date().getTime() < expiresAt;
-}
-
-function handleAuthentication() {
-  webAuth.parseHash(function(err, authResult) {
-    if (authResult && authResult.accessToken && authResult.idToken) {
-      window.location.hash = '';
-      setSession(authResult);
-      loginBtn.style.display = 'none';
-      homeView.style.display = 'inline-block';
-    } else if (err) {
-      homeView.style.display = 'inline-block';
-      console.log(err);
-      alert(
-        'Error: ' + err.error + '. Check the console for further details.'
-      );
-    }
-    displayButtons();
-  });
-}
-
-function displayButtons() {
-  if (isAuthenticated()) {
-    loginBtn.style.display = 'none';
-    logoutBtn.style.display = 'inline-block';
-    loginStatus.innerHTML = 'You are logged in!';
-  } else {
-    loginBtn.style.display = 'inline-block';
-    logoutBtn.style.display = 'none';
-    loginStatus.innerHTML =
-      'You are not logged in! Please log in to continue.';
+  function setSession(authResult) {
+    // Set the time that the access token will expire at
+    var expiresAt = JSON.stringify(
+      authResult.expiresIn * 1000 + new Date().getTime()
+    );
+    localStorage.setItem('access_token', authResult.accessToken);
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('expires_at', expiresAt);
   }
-}
 
-handleAuthentication();
+  function logout() {
+    // Remove tokens and expiry time from localStorage
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+    displayButtons();
+  }
+
+  function isAuthenticated() {
+    // Check whether the current time is past the
+    // access token's expiry time
+    var expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    return new Date().getTime() < expiresAt;
+  }
+
+  function displayButtons() {
+    var loginStatus = document.querySelector('.container h4');
+    if (isAuthenticated()) {
+      loginBtn.style.display = 'none';
+      logoutBtn.style.display = 'inline-block';
+      profileViewBtn.style.display = 'inline-block';
+      orderBtn.style.display = 'inline-block';
+      loginStatus.innerHTML =
+        'You are logged in! You can now view your profile area.';
+    } else {
+      homeView.style.display = 'inline-block';
+      loginBtn.style.display = 'inline-block';
+      logoutBtn.style.display = 'none';
+      profileViewBtn.style.display = 'none';
+      profileView.style.display = 'none';
+      loginStatus.innerHTML =
+        'You are not logged in! Please log in to continue.';
+    }
+  }
+
+  function getProfile() {
+    if (!userProfile) {
+      var accessToken = localStorage.getItem('access_token');
+
+      if (!accessToken) {
+        console.log('Access token must exist to fetch profile');
+      }
+
+      webAuth.client.userInfo(accessToken, function(err, profile) {
+        if (profile) {
+          userProfile = profile;
+          displayProfile();
+        }
+      });
+    } else {
+      displayProfile();
+    }
+  }
+
+  function displayProfile() {
+    // display the profile
+    document.querySelector('#profile-view .nickname').innerHTML =
+      userProfile.nickname;
+    document.querySelector(
+      '#profile-view .full-profile'
+    ).innerHTML = JSON.stringify(userProfile, null, 2);
+    document.querySelector('#profile-view img').src = userProfile.picture;
+  }
+
+  function handleAuthentication() {
+    webAuth.parseHash(function(err, authResult) {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        window.location.hash = '';
+        setSession(authResult);
+        loginBtn.style.display = 'none';
+        homeView.style.display = 'inline-block';
+      } else if (err) {
+        homeView.style.display = 'inline-block';
+        console.log(err);
+        alert(
+          'Error: ' + err.error + '. Check the console for further details.'
+        );
+      }
+      displayButtons();
+    });
+  }
+
+  handleAuthentication();
 });
-
--- server.js
+++ server.js
@@ -11,70 +11,13 @@ app.use(express.static(__dirname));
 var bodyParser = require('body-parser')
 app.use(bodyParser.json());
 
-//set up DB connection
-var mongoose = require('mongoose');
-const {ObjectId} = require('mongodb');
-var {custProfile} = require('./custprofile');
-
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/STLProvel');
-
-//post a single profile
-app.post('/profiles', (request, response) => {
-      console.log(request.body);
-      var profile = new custProfile({
-          username: request.body.username,
-          email: request.body.email
-      })
-
-      profile.save().then((doc) => {
-          response.status(200).send(doc)
-
-      }, (error) => {
-          response.status(400).send(error);
-      });
-});
-
-//get all profiles
-app.get('/profiles', (request, response) => {
-    custProfile.find().then((profiles) => {
-        response.send({profiles});
-
-    }, (error) => {
-        response.status(400).send(error);
-    });
-});
-
-//get by specific DB _ID
-app.get('/profiles/:id', (request, response) => {
-    var id = request.params.id;
-    if(ObjectId.isValid(request.params.id))
-    {
-        custProfile.findById(request.params.id).then((profile) => {
-          response.status(200).send;
-          response.send({profile});
-
-         }, (error) => {
-             response.status(400).send(error);
-            console.log('unable to fetch profile');
-        })
-
-    } else {
-        console.log('invalid id');
-        response.status(400).send('invalid id');
-    }
-d
-});
-
 app.get('/', function(request, response) {
-    console.log ('HTML INCOMING');
     response.sendFile(path.join(__dirname + '/index.html'));
 });
 
-
 //start the webserver
 app.listen(port, ()=> {
-    console.log(`Pizza baking on port ${port}`);
+    console.log(`Pizza 42 running on port ${port}`);
 });
 
 
